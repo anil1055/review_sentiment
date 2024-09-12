3d73f7bbb7b619f1465355e61654e60621774bda
Anil Guven <anilguven1055@gmail.com>
2024-09-12 13:55:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,26 @@ import requests
 
 st.set_page_config(page_title="Turkish Review Analysis - via AG", page_icon='🦙')
 
-MODEL_IMAGES = {
-    "anilguven/albert_tr_turkish_movie_reviews": "https://em-content.zobj.net/source/twitter/376/llama_1f999.png",  # Add the emoji for the Meta-Llama model
-    "anilguven/distilbert_tr_turkish_movie_reviews": "https://em-content.zobj.net/source/twitter/376/llama_1f999.png",
-    "anilguven/electra_tr_turkish_movie_reviews": "https://em-content.zobj.net/source/twitter/376/tornado_1f32a-fe0f.png",
-    "anilguven/bert_tr_turkish_movie_reviews": "https://em-content.zobj.net/source/twitter/376/tornado_1f32a-fe0f.png",
+MODEL_MOVIE = {
+    "albert": "anilguven/albert_tr_turkish_movie_reviews",  # Add the emoji for the Meta-Llama model
+    "distilbert": "anilguven/distilbert_tr_turkish_movie_reviews",
+    "electra": "anilguven/electra_tr_turkish_movie_reviews",
+    "bert": "anilguven/bert_tr_turkish_movie_reviews",
 }
+
+from simpletransformers.classification import ClassificationModel
+
+model_b = ClassificationModel("bert", "anilguven/bert_tr_turkish_movie_reviews")
 
 # Create a mapping from formatted model names to their original identifiers
 def format_model_name(model_key):
-    parts = model_key.split('/')
-    name_parts = parts[1]
-
-    # General formatting for other models
+    name_parts = model_key
     formatted_name = ' '.join(name_parts)  # Join them into a single string with title case
     return formatted_name
 
 formatted_names_to_identifiers = {
-    format_model_name(key): key for key in MODEL_IMAGES.keys()
+    format_model_name(key): key for key in MODEL_MOVIE.keys()
 }
-
 
 
 # Debug to ensure names are formatted correctly
@@ -37,10 +37,7 @@ selected_formatted_name = st.sidebar.radio(
 
 selected_model = formatted_names_to_identifiers[selected_formatted_name]
 
-if MODEL_IMAGES[selected_model].startswith("http"):
-    st.image(MODEL_IMAGES[selected_model], width=90)
-else:
-    st.write(f"Model Icon: {MODEL_IMAGES[selected_model]}", unsafe_allow_html=True)
+#st.image(MODEL_IMAGES[selected_model], width=90)
 
 # Display the selected model using the formatted name
 model_display_name = selected_formatted_name  # Already formatted
@@ -72,9 +69,9 @@ comment = st.text_input("Enter your text for analysis")#User input
 
 st.text('')
 if st.button("Submit for Analysis"):#User Review Button
-	st.text("Positive")
-	
-st.text('')#This shows up the Review as positive,negative or neutral.
+	result = model_b.predict([comment])
+	st.text(result)
+
 
 if "api_key" not in st.session_state:
     st.session_state.api_key = ""
