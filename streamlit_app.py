8e7ba9bd37ee50021917d5eb3ee45c97ad5508d1
Anil Guven <anilguven1055@gmail.com>
2024-09-12 14:33:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,12 @@ comment = st.text_input("Enter your text for analysis")#User input
 
 st.text('')
 if st.button("Submit for Analysis"):#User Review Button
-	label, score = pipe(comment)[0]
-	if label == "LABEL_0": result = "Negative"
-	else: label = "Positive"
-	st.text(label + "with " + str(score["score"]*100) + " accuracy")
+	result = pipe(comment)[0]
+	st.text(result)
+	#label, score = pipe(comment)[0]
+	#if label == "LABEL_0": result = "Negative"
+	#else: label = "Positive"
+	#st.text(label + "with " + str(score["score"]*100) + " accuracy")
 
 
 if "api_key" not in st.session_state:
