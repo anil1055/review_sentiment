7f491011f5cab4ee546d20c43d3bfb007bc8649e
Anil Guven <anilguven1055@gmail.com>
2024-09-12 15:16:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,11 @@ selected_formatted_name = st.sidebar.radio(
 access_token = "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"
 
 selected_model = MODEL_MOVIE[selected_formatted_name]
-st.text(selected_model)
-pipe = pipeline("text-classification", model=selected_model, token=access_token)
+#pipe = pipeline("text-classification", model=selected_model, token=access_token)
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained(selected_model)
+pipe = AutoModelForSequenceClassification.from_pretrained(model=selected_model, token=access_token)
 
 #st.image(MODEL_IMAGES[selected_model], width=90)
 
