335e3f3cdd1ee52083a1734542961571c1d41445
Anil Guven <anilguven1055@gmail.com>
2024-09-14 00:14:20 +0300

    Update pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,18 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
 
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles
+# and icons should be
+show_pages(
+    [
+        Page("streamlit_app.py", "Movie Reviews", "🏠"),
+    ]
+)
 st.set_page_config(page_title="Turkish Review Analysis - via AG", page_icon='📖')
 st.header("📖Movie Review Analysis - TR")
-st.title("Movie Reviews")
 
 MODEL_MOVIE = {
     "albert": "anilguven/albert_tr_turkish_movie_reviews",  # Add the emoji for the Meta-Llama model
