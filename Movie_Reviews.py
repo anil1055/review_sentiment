f7ea7286c2523f4edaf01fbc873e775a15595fb2
Anil Guven <anilguven1055@gmail.com>
2024-09-14 00:25:17 +0300

    Update pages

--- Movie_Reviews.py
+++ Movie_Reviews.py
@@ -1,16 +1,5 @@
 import streamlit as st
-from st_pages import Page, show_pages, add_page_title
 
-# Optional -- adds the title and icon to the current page
-add_page_title()
-
-# Specify what pages should be shown in the sidebar, and what their titles
-# and icons should be
-show_pages(
-    [
-        Page("streamlit_app.py", "Movie Reviews", "🏠"),
-    ]
-)
 st.set_page_config(page_title="Turkish Review Analysis - via AG", page_icon='📖')
 st.header("📖Movie Review Analysis - TR")
 
