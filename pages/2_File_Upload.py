26b95e796dfa5c8451a5f3e14c00a40df3dbdaea
Anil Guven <anilguven1055@gmail.com>
2024-09-13 17:58:04 +0300

    Update 2_File_Upload.py

--- pages/2_File_Upload.py
+++ pages/2_File_Upload.py
@@ -51,9 +51,9 @@ try:
     if uploaded_file.name.lower().endswith(".csv"):
         file = uploaded_file
     elif uploaded_file.name.lower().endswith(".txt"):
-        text = uploaded_file.readlines().decode("utf-8", errors="replace")
-        datas = [line for line in text]
-        st.text(datas)
+        text = uploaded_file.read().decode("utf-8", errors="replace")
+        datas = [line for line in uploaded_file]
+        st.text(text)
     else:
         raise NotImplementedError(f"File type {uploaded_file.name.split('.')[-1]} not supported")
 except Exception as e:
